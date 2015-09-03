c189e4b5bd61b1986f6160da6c114484f6a1cfcb
Wilhelm M <wminf@users.noreply.github.com>
2015-09-03 12:02:04 +0200

    requirements of simulate from exercise 21

---
+++ elastic_pendulum.py
@@ -0,0 +1,8 @@
+def simulate(
+    beta=0.9,                 # dimensionless parameter
+    Theta=30,                 # initial angle in degrees
+    epsilon=0,                # initial stretch of wire
+    num_periods=6,            # simulate for num_periods
+    time_steps_per_period=60, # time step resolution
+    plot=True,                # make plots or not
+    ):
